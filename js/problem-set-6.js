da4a9a47254c6fcc24ca5aaaed9bd5dfd06320ef
Griffin Cederquist <killergs2@live.com>
2018-12-17 21:49:26 -0500

    thank the lord jesus for w3schools and mdn

--- js/problem-set-6.js
+++ js/problem-set-6.js
@@ -1,5 +1,5 @@
 /*
- * Hello. 2 points.
+ * Hello. 2 pointS.
  *
  * Write a function that draws "Hello, World!" on the canvas. You should
  * begin drawing at [10, 50], relative to the canvas, and your text should
@@ -19,7 +19,7 @@ function sayHello() {
 }
 
 /*
- * Rectangle. 3 points.
+ * Rectangle. 3 pointS.
  *
  * Write a function that draws a rectangle on the canvas. You should prompt
  * the user for the height and width. You should also prompt the user for the
@@ -64,7 +64,7 @@ function drawRectangle() {
 }
 
 /*
- * Color. 3 points.
+ * Color. 3 pointS.
  *
  * Write a function that draws a rectangle on the canvas. By default, the
  * rectangle will have a height and width of 50px and 100px, respectively. It
@@ -102,12 +102,12 @@ function drawColoredRectangle() {
   }
 
 /*
- * Triangle. 5 points.
+ * Triangle. 5 pointS.
  *
  * Write a function that draws a right triangle on the canvas. The triangle
  * should be outlined only, not filled. You'll need to prompt the user for the
  * lengths of each of the sides. By default, position your triangle so that
- * its leftmost and topmost points have X- and Y-coordinates of 10.
+ * its leftmost and topmost pointS have X- and Y-coordinates of 10.
  *
  * When drawing your right triangles, the left side shall be designated as the
  * first side and will be smallest of the three side lengths. The bottom side
@@ -131,26 +131,41 @@ function drawColoredRectangle() {
  */
 
 function drawTriangle() {
-    var canvas = document.getElementById('canvas4');
-    if (canvas.getContext) {
-      var ctx = canvas.getContext('2d');
-line1 = prompt("Side 1");
-line2 = prompt("Side 2");
-line3 = prompt("Side 3")
-      ctx.beginPath();
-      ctx.moveTo(10, 10);
-      ctx.lineTo((10), (line1+10));
-      ctx.lineTo((line1+10), (line2+10));
-      ctx.closePath();
-      ctx.stroke();
-      console.log(line1);
-      console.log(line2);
-      console.log(line3);
-    }
+  let x=10;
+  let y=10;
+  let side1;
+  let side2;
+  let side3;
+  let canvas = document.getElementById("canvas4");
+  let context = canvas.getContext("2d");
+  context.clearRect(0, 0, canvas.width, canvas.height);
+  while(true){
+	side1=Number(prompt("Enter side 1 length"));
+	side2=Number(prompt("Enter side 2 length"));
+	side3=Number(prompt("Enter side 3 length"));
+	if(((side1**2) + (side2**2) == (side3**2)) && side1>0 && side2>0 && side3>0 && canvas.width-x-side1>=0 && canvas.height-y-side2>=0){
+		break;
+	}else{
+		alert("That's not a valid right triangle.")
+	}
 }
+context.beginPath();
+ context.moveTo(x,y);
+ context.lineTo(x,y+side1);
+ context.stroke();
 
+ context.beginPath();
+ context.moveTo(x,y+side1);
+ context.lineTo(x+side2,y+side1);
+ context.stroke();
+
+ context.beginPath();
+ context.moveTo(x,y);
+ context.lineTo(x+side2,y+side1);
+context.stroke();
+}
 /*
- * Smile. 7 points.
+ * Smile. 7 pointS.
  *
  * Write a function that draws a smiley face on the canvas. The head should
  * be a perfect circle with a radius specified by the user. The eyes should
@@ -169,16 +184,45 @@ line3 = prompt("Side 3")
  */
 
 function drawSmileyFace() {
+  let radius;
+    let canvas = document.getElementById('canvas5');
+    let ctx = canvas.getContext('2d');
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+  	while(true){
+  		radius=Number(prompt("Please enter a reasonable radius."));
+  		if(radius>=1 && radius<=canvas.width && Number.isInteger(radius)){
+  			break;
+  		}
+  	}
+    let x=canvas.width;
+    let y=canvas.height;
+    let eyeRadius=radius*.1
+    let mouthRadius=radius*.7
+    ctx.beginPath();
+    ctx.arc(x/2, y/2, radius, 0, Math.PI*2);
+    ctx.stroke();
 
-}
+    ctx.beginPath();
+    ctx.arc(x/2 - radius/3, y/2-radius/4, eyeRadius,0, Math.PI*2);
+    ctx.stroke();
+
+    ctx.beginPath();
+    ctx.arc(x/2+radius/3, y/2-radius/4, eyeRadius,0, Math.PI*2);
+    ctx.stroke();
+
+    ctx.beginPath();
+    ctx.arc(x/2, y/2, mouthRadius,0, Math.PI);
+    ctx.stroke();
+  }
+
 
 /*
- * Star. 9 points.
+ * Star. 9 pointS.
  *
  * Write a function that draws a five-point star on the canvas. Prompt the
  * user for the outer radius (i.e., a circle that would connect each of the
- * star's outer points) and inner radius (i.e., a circle that would connect
- * each of the star's inner points). The center of the star should be placed
+ * star's outer pointS) and inner radius (i.e., a circle that would connect
+ * each of the star's inner pointS). The center of the star should be placed
  * at [125, 125].
  *
  * You'll need to use the appropriate Canvas API methods to do this. If you're
@@ -191,11 +235,42 @@ function drawSmileyFace() {
  */
 
 function drawStar() {
+  let canvas = document.getElementById('canvas6');
+      let ctx = canvas.getContext('2d');
+      ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-}
+        let outerRadius=Number(prompt("Enter a valid outer radius"));
+        let innerRadius=Number(prompt("Enter a valid inner radius"));
+        if (outerRadius>=innerRadius && canvas.width>=outerRadius+125 && canvas.height>=outerRadius+125 && innerRadius>=1 && outerRadius>=1){
+          let pointS=5;
+          let outerx=[];
+          let outery=[];
+          let innerx=[];
+          let innery=[];
+          for(let i=0;i<pointS;i++){
+            outerx.push(Math.cos((Math.PI*2*i)/pointS-(Math.PI/2))*outerRadius+125);
+            outery.push(Math.sin((Math.PI*2*i)/pointS-(Math.PI/2))*outerRadius+125);
+            innerx.push(Math.cos(((Math.PI*2*i)/pointS)-(Math.PI/2)+(Math.PI/pointS))*innerRadius+125);
+            innery.push(Math.sin(((Math.PI*2*i)/pointS)-(Math.PI/2)+(Math.PI/pointS))*innerRadius+125);
+          }
+          ctx.beginPath();
+          ctx.moveTo(outerx[0], outery[0]);
+          for(let j=0;j<outerx.length;j++){
+            ctx.lineTo(innerx[j], innery[j]);
+            ctx.lineTo(outerx[j+1], outery[j+1]);
+          }
+          ctx.lineTo(outerx[0], outery[0]);
+          ctx.stroke();
+          ctx.closePath();
+      }
+      else{
+          alert('Invalid inputs detected.');
+        }
+  }
+
 
 /*
- * Stop Sign. 7 points.
+ * Stop Sign. 7 pointS.
  *
  * Write a function that draws a stop sign. The stop sign should be outlined
  * in black, but filled red. At the center of the stop sign, the same height
@@ -210,11 +285,42 @@ function drawStar() {
  */
 
 function drawStopSign() {
+  let canvas = document.getElementById('canvas7');
+   let ctx = canvas.getContext('2d');
+   ctx.clearRect(0, 0, canvas.width, canvas.height);
+   let sidelength=80;
+   let center=[10+(sidelength)/2+sidelength/Math.sqrt(2), 10+(sidelength/2)+(sidelength/Math.sqrt(2))]
+   console.log(center)
+   let pointS=8;
+   let pointX=[];
+   let pointY=[];
 
-}
+   for(let i=0;i<pointS;i++){
+     pointX.push(Math.cos(((Math.PI*2*i)/pointS)-Math.PI/8)*100+center[0]);
+     pointY.push(Math.sin(((Math.PI*2*i)/pointS)-Math.PI/8)*100+center[1]);
+   }
+   ctx.beginPath();
+   ctx.moveTo([pointX][0], pointY[0]);
+   for(let j=0;j<pointX.length;j++){
+     ctx.lineTo(pointX[j], pointY[j]);
+   }
+   ctx.lineTo(pointX[0], pointY[0]);
+   ctx.stroke();
+
+   ctx.fillStyle="red";
+   ctx.fill();
+   ctx.closePath();
+   ctx.beginPath();
+   ctx.textAlign="center";
+   ctx.font="70px Lucida Console";
+   ctx.fillStyle="white";
+   ctx.fillText("STOP", center[0], center[1]+22);
+   ctx.closePath()
+ }
+
 
 /*
- * Pyramid. 7 points.
+ * Pyramid. 7 pointS.
  *
  * Write a function that draws a block pyramid, where the user specifies the
  * side length of each block. By default, we'll draw a pyramid with a base
@@ -232,11 +338,27 @@ function drawStopSign() {
  */
 
 function drawPyramid() {
-
+  let canvas = document.getElementById('canvas8');
+    let ctx = canvas.getContext('2d');
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    let sideLength=Number(prompt("Please enter desired side length."));
+    let x=10;
+    let y=canvas.height-10;
+    let i=0;
+    lineNumber=1;
+    while(i<5){
+      for(let j=0+lineNumber;j<=5;j++){
+        ctx.strokeRect(x,y-sideLength,sideLength,sideLength);
+        x+=sideLength;
+      }
+      x=10+(sideLength/2)*lineNumber;
+      y-=sideLength;
+      lineNumber++;
+  i++;
 }
-
+}
 /*
- * House. 7 points.
+ * House. 7 pointS.
  *
  * Write a function that draws a two-story house. The house should have a
  * rectangular frame, a front door, two square windows on the first floor,
@@ -265,5 +387,59 @@ function drawPyramid() {
  */
 
 function drawHouse() {
+  let canvas = document.getElementById('canvas9');
+    let ctx = canvas.getContext('2d');
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-}
+    while(true){
+    doorColor=prompt("Please enter desired door color.");
+    houseColor=prompt("Please enter a desired house color.");
+    if((doorColor=="brown" || doorColor=="blue" || doorColor=="green" || doorColor=="orange" || doorColor=="purple" || doorColor=="red" || doorColor=="yellow")
+    && (houseColor=="brown" || houseColor=="blue" || houseColor=="green" || houseColor=="orange" || houseColor=="purple" || houseColor=="red" || houseColor=="yellow")) {
+      break;
+    }
+    else{
+        alert("You have entered one or more invalid colors. Please try again.")
+    }
+  }
+  let x=150;
+  let lengthHouse=576;
+  let heightHouse=400;
+  let y=canvas.height-heightHouse-10;
+  ctx.beginPath();
+
+  ctx.fillStyle=houseColor;
+  ctx.fillRect(x,y,lengthHouse,heightHouse);
+  ctx.strokeRect(x,y,lengthHouse,heightHouse);
+
+  ctx.fillStyle=doorColor;
+  ctx.fillRect(x+(lengthHouse/2)-30,y+300,60,100);
+  ctx.strokeRect(x+(lengthHouse/2)-30,y+300,60,100);
+  ctx.stroke();
+
+
+  ctx.fillStyle="gray";
+  ctx.moveTo(x,y);
+  ctx.lineTo(x+286,150);
+  ctx.lineTo(x+lengthHouse,y);
+  ctx.lineTo(x,y);
+  ctx.fill();
+  ctx.stroke();
+
+  ctx.fillStyle="#ADD8E6";
+  ctx.fillRect(300,y+100, 50, 50);
+  ctx.fillRect(526,y+100, 50, 50);
+  ctx.strokeRect(300,y+100, 50, 50);
+  ctx.strokeRect(526,y+100, 50, 50);
+  ctx.fillRect(300,y+200, 50, 50);
+  ctx.fillRect(526,y+200, 50, 50);
+  ctx.strokeRect(300,y+200, 50, 50);
+  ctx.strokeRect(526,y+200, 50, 50);
+  ctx.closePath();
+
+  ctx.beginPath();
+  ctx.fillStyle='black';
+  ctx.arc(450, 700, 6, 0, Math.PI*2);
+  ctx.fill();
+  ctx.closePath();
+  }
